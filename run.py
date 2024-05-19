1f2bd7e1aa22e71d4d2012841848a33c94f93281
Toby Jefferies <toby_jefferies@hotmail.com>
2024-05-19 23:37:36 +0000

    fix some errors, main test still fail

--- run.py
+++ run.py
@@ -46,42 +46,40 @@ def place_ships(board):
    loops through length of ships
    """
    for ship_length in LENGTH_OF_SHIPS:
-    """
-    loops until ship fits and does not overlap
-    """
-    while True:
-        if board == COMPUTER_BOARD:
-            orientation, row, column = random.choice(["H", "V"]), random.randint(0,7), random.randint(0,7)
-            if check_ship_fit(ship_length, row, column, orientation):
-                #check if ships overlap
-                if ship_overlaps(board, row, column, orientation, ship_length) == False:
-                    #place ship
-                    if orientation == "H":
-                        for i in range(column, column + ship_length):
-                            board[row][i] = "X"
-                    else:
-                        for i in range(row, row + ship_length):
-                            board[i][column] = "X"
-                    break
-            else:
-                place_ship = True
-                print('Place the ship with a length of ' + str(ship_length))
-                row, column, orientation = user_input(place_ship)
+    #loops until ship fits and does not overlap
+        while True:
+            if board == COMPUTER_BOARD:
+                orientation, row, column = random.choice(["H", "V"]), random.randint(0,7), random.randint(0,7)
                 if check_ship_fit(ship_length, row, column, orientation):
-                    #check if ship overlaps
+                    #check if ships overlap
                     if ship_overlaps(board, row, column, orientation, ship_length) == False:
                         #place ship
                         if orientation == "H":
                             for i in range(column, column + ship_length):
-                                board[i][row] = "X"
+                                board[row][i] = "X"
                         else:
                             for i in range(row, row + ship_length):
                                 board[i][column] = "X"
-                        print_board(PLAYER_BOARD)
                         break
+                else:
+                    place_ship = True
+                    print('Place the ship with a length of ' + str(ship_length))
+                    row, column, orientation = user_input(place_ship)
+                    if check_ship_fit(ship_length, row, column, orientation):
+                        #check if ship overlaps
+                        if ship_overlaps(board, row, column, orientation, ship_length) == False:
+                            #place ship
+                            if orientation == "H":
+                                for i in range(column, column + ship_length):
+                                    board[i][row] = "X"
+                            else:
+                                for i in range(row, row + ship_length):
+                                    board[i][column] = "X"
+                            print_board(PLAYER_BOARD)
+                            break
 
 
-def check_ship_fit(SHIP_LENGTH,row, column, orientation):
+def check_ship_fit(SHIP_LENGTH, row, column, orientation):
     if orientation == "H":
         if column + SHIP_LENGTH > 8:
             return False
@@ -145,7 +143,7 @@ def user_input(place_ship):
         try:
             column = input ("Enter the column of the ship: ").upper()
             if column in "ABCDEFGH":
-                column - LETTERS_TO_NUMBERS[column]
+                column = LETTERS_TO_NUMBERS[column]
                 break
         except KeyError:
             print("Enter a valid letter between A-H")
@@ -172,15 +170,17 @@ def turn(board):
         elif COMPUTER_BOARD[row][column] == "X":
             board[row][column] = "X"
         else:
-            row, column = random.randint(0,7), random.randint(0,7)
-            if board[row][column] == "-":
-                turn(board)
-            elif board[row][column] == "X":
-                turn(board)
-            elif PLAYER_BOARD[row][column] == "X":
-                board[row][column] = "X"
-            else:
-                board[row][column] = "-"
+            board[row][column] = "X"
+    else:
+        row, column = random.randint(0,7), random.randint(0,7)
+        if board[row][column] == "-":
+            turn(board)
+        elif board[row][column] == "X":
+            turn(board)
+        elif PLAYER_BOARD[row][column] == "X":
+            board[row][column] = "X"
+        else:
+            board[row][column] = "-"
 
 
 place_ships(COMPUTER_BOARD)
